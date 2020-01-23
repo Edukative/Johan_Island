263bc6214615836fe29c47b82d588915828b3675
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-23 19:34:38 +0100

    SpawnManager Created
    We created a spawnmanager

---
+++ Island/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private Rigidbody enemyRB;
+    public float speed;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirections = (player.transform.position - this.transform.position).normalized;
+        enemyRB.AddForceAtPosition(lookDirections * speed * Time.deltaTime, player.transform.position, ForceMode.Impulse);
+    }
+}
---
+++ Island/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    float spawnRange = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(enemyPrefab, GenerateRandomPosition(), enemyPrefab.transform.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    Vector3 GenerateRandomPosition()
+    {
+        float spawnRandomX = Random.Range(-spawnRange, spawnRange);
+        float spawnRandomZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnRandomPosition = new Vector3(spawnRandomX, 0, spawnRandomZ);
+        Debug.Log("The virus is multiplicating!" + spawnRandomPosition);
+    }
+}
