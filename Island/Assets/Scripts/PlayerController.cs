d75ec4970c730882eb7153ad2bd9180f9aff0879
Edukative <56083723+Edukative@users.noreply.github.com>
2020-02-06 19:34:25 +0100

    Powerup
    Added push powerup

--- Island/Assets/Scripts/PlayerController.cs
+++ Island/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,21 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     public float speed;
     GameObject focalPoint;
+
+    //powerup values
+
+    bool hasPowerup;
+    public float powerupStrength = 15;
+    GameObject powerupIndicator;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+
+        //powerup
+        powerupIndicator = GameObject.Find("PowerupIndicator");
+        powerupIndicator.SetActive(false);
 
     }
 
@@ -19,6 +29,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime, ForceMode.Force);
+
+        //powerUp
+        powerupIndicator.transform.position = transform.position;
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+            powerupIndicator.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRB = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 pushAwayEnemy = (other.gameObject.transform.position - transform.position);
+            Debug.Log("We've just hitted " + other.gameObject + " with the " + hasPowerup + " powerup!");
+            enemyRB.AddForce(pushAwayEnemy * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
--- Island/Assets/Scripts/SpawnManager.cs
+++ Island/Assets/Scripts/SpawnManager.cs
@@ -10,22 +10,31 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        Instantiate(enemyPrefab, GenerateRandomPosition(), enemyPrefab.transform.rotation);
-
+        spawnEnemyWave(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     Vector3 GenerateRandomPosition()
     {
         float spawnRandomX = Random.Range(-spawnRange, spawnRange);
         float spawnRandomZ = Random.Range(-spawnRange, spawnRange);
         Vector3 spawnRandomPosition = new Vector3(spawnRandomX, 0, spawnRandomZ);
-        Debug.Log("The virus is multiplicating!" + spawnRandomPosition);
+        Debug.Log("Spreading!" + spawnRandomPosition);
+        return spawnRandomPosition;
+    }
+
+    void spawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, GenerateRandomPosition(), enemyPrefab.transform.rotation);
+            Rigidbody enemyRB = enemy.GetComponent<Rigidbody>();
+            enemyRB.mass = Random.Range(-min)
+        }
     }
 }
